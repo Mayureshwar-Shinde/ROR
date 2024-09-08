3003099fe6ce90ed5c4bf9c6f71a113ae0a6a8fd
mayureshwar.shinde <mayureshwarshinde@gmail.com>
2024-09-08 22:22:31 +0530

    added specs for user and sidemenu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :age, :date_of_birth, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validate :date_validation
+
+  private
+
+  def date_validation
+    return if date_of_birth.nil? || date_of_birth < Date.today
+    errors.add(:date_of_birth, 'must be in the past')
+  end
 end
